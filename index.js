520828d29c4b28c92f208ce6e40bc53511a987eb
Trey Griffith <trey.griffith@gmail.com>
2015-04-21 23:56:22 -0700

    use updated node client with simpler syntax for specifying a type

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ var Immutio = require('immutio-client'),
     paste = fs.readFileSync(path.resolve(__dirname, './paste.html')),
     im = new Immutio();
 
-var pasteBlob = new Immutio.Blob(null, paste, im);
-pasteBlob.type = "text/html";
-
-pasteBlob.store(function (err) {
+im.store(paste, "text/html", function (err, id) {
   if(err) throw err;
 
-  console.info("stored paste at " + pasteBlob.location());
+  var location = "/blobs/" + id;
+  console.info("stored paste at " + location);
 
-  exec('heroku config:set PASTE_URL=' + pasteBlob.location() + ' -a immut');
+  exec('heroku config:set PASTE_URL=' + location + ' -a immut');
 
   console.info("PASTE_URL updated");
 });
